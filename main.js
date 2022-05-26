3b687a80d1d8648ca4422c0b571d7e164d5fbfa1
flerda <fglerda@gmail.com>
2022-05-26 12:18:17 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,6 @@
+// solicitar contraseña ciclo While:
+let pass = prompt("Introduzca su contraseña");
+while (pass != "1234") {
+    pass = prompt("Contraseña incrooecta, vuelva a intentar")
+};
+alert("Bienvenido...")
